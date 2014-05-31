a1fb66655526a6f7281b8fe06ea5bc84bc36e024
Dani Criado <dani.criado.casas@gmail.com>
2014-05-31 13:21:53 +0200

    functions save_pde_algorithm_command

--- lang/ca/lang.php
+++ lang/ca/lang.php
@@ -7,11 +7,17 @@
 
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :
-$lang['saveFileIncorrect']    = 'No s\'ha pogut dessar el fitxer correctament';
-$lang['saveFileCorrect']      = 'Fitxer dessat correctament';
-$lang['filenameExists']       = 'El nom del fitxer ja existeix';
-$lang['filenameNotExists']    = 'El nom del fitxer està disponible';
-$lang['undefinedCommand']     = 'Comanda no definida';
-$lang['unexpectedError']      = 'Error inesperat';
-$lang['loadedXml']            = 'Els algorismes s\'han carregat correctament';
-$land['unloadedXml']          = 'No s\'ha pogut carregar el llistat dels algorismes';
+$lang['saveFileIncorrect']      = 'No s\'ha pogut dessar el fitxer correctament';
+$lang['saveFileCorrect']        = 'Fitxer dessat correctament';
+$lang['filenameExists']         = 'El nom del fitxer ja existeix';
+$lang['filenameNotExists']      = 'El nom del fitxer està disponible';
+$lang['undefinedCommand']       = 'Comanda no definida';
+$lang['unexpectedError']        = 'Error inesperat';
+$lang['loadedXml']              = 'Els algorismes s\'han carregat correctament';
+$land['unloadedXml']            = 'No s\'ha pogut carregar el llistat dels algorismes';
+
+
+//save_pde_algorithm
+$lang['algorithmExists']        = 'Aquest nom de fitxer de l\'algorisme ja existeix';
+$lang['algorithmNotExists']     = 'Aquest nom de fitxer de l\'algorisme encara no existeix';
+$lang['unloaderAlgorithm']      = 'No s\'ha carregat el fitxer de l\'algorisme';
