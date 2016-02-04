8bcb96a25717f357df1730e71038f1d79bae3110
Xavier García <xaviergaro.dev@gmail.com>
2016-02-04 21:10:29 +0100

    Minor fix

--- abstract_command_class.php
+++ abstract_command_class.php
@@ -265,7 +265,7 @@ abstract class abstract_command_class extends DokuWiki_Plugin {
         if (isset($this->params['iframe'])) {
           $jsonResponse = "<html><body><textarea>" . $jsonResponse . "</textarea></body></html>";   
        }
-        
+        return $jsonResponse;
     }
 
     /**
