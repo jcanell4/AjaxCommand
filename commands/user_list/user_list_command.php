ef7f2015792eddfb236fe5108f7639eeb6b23481
jcanell4 <jcanell4@xtec.cat>
2017-04-25 21:26:14 +0200

    user_list command

--- commands/user_list/user_list_command.php
+++ commands/user_list/user_list_command.php
@@ -12,6 +12,7 @@ require_once(DOKU_COMMAND . 'AjaxCmdResponseGenerator.php');
 require_once(DOKU_COMMAND . 'JsonGenerator.php');
 require_once(DOKU_COMMAND . 'abstract_command_class.php');
 require_once(DOKU_COMMAND . 'requestparams/PageKeys.php');
+require_once(DOKU_PLUGIN . 'wikiiocmodel/actions/UserListAction.php');
 
 /**
  * Class edit_command
@@ -27,26 +28,30 @@ class user_list_command extends abstract_command_class {
         $this->types[PageKeys::KEY_DO] = abstract_command_class::T_STRING;
         $this->types[PageKeys::KEY_FILTER] = abstract_command_class::T_STRING;
         $this->types[PageKeys::KEY_START_POS] = abstract_command_class::T_INTEGER;
-        $this->types[PageKeys::KEY_PROJECT] = abstract_command_class::T_INTEGER;
+        $this->types[PageKeys::KEY_PROJECT] = abstract_command_class::T_STRING;
 
 //        $defaultValues = ['do' => 'edit'];
 //        $this->setParameters($defaultValues);
     }
     
     protected function getDefaultResponse($response, &$responseGenerator) {
+        $responseGenerator->addArrayTypeResponse($response);
         
     }
 
     protected function process() {
-        if(!isset($this->types[PageKeys::KEY_DO]) && 
+        if(!isset($this->params[PageKeys::KEY_DO]) && 
                 isset($this->params[PageKeys::KEY_PROJECT]) && 
                 isset($this->params[PageKeys::KEY_ID])){
-            $this->types[PageKeys::KEY_DO] = "ofAProject";
-        }elseif(!isset($this->types[PageKeys::KEY_DO]) && isset($this->params[PageKeys::KEY_ID])){
-            $this->types[PageKeys::KEY_DO] = "byPagePermision";
-        }elseif(!isset($this->types[PageKeys::KEY_DO])){
-            $this->types[PageKeys::KEY_DO] = "byName";            
+            $this->params[PageKeys::KEY_DO] = UserListAction::OF_A_PROJECT;
+        }elseif(!isset($this->params[PageKeys::KEY_DO]) && isset($this->params[PageKeys::KEY_ID])){
+            $this->params[PageKeys::KEY_DO] = UserListAction::BY_PAGE_PERMSION;
+        }elseif(!isset($this->params[PageKeys::KEY_DO])){
+            $this->params[PageKeys::KEY_DO] = UserListAction::BY_NAME;            
         }
         
+        $action = new UserListAction($this->modelWrapper->getPersistenceEngine());
+        $response = $action->get($this->params);
+        return $response;
     }
 }
