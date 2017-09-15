5999f67b6b551eb0749f059e5ddaad7a3879edfa
professor <professor@debian8>
2017-09-15 12:17:28 +0200

    Merge master i T066

---
+++ defkeys/GlobalKeys.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * GlobalKeys: difinició de constants genèriques d'àmbit global
+ */
+
+class GlobalKeys {
+    const SHORTCUT_PAGE_NAME = "dreceres";
+    const TEMPLATE_SHORTCUTS_NS = "plantilles:user:".self::SHORTCUT_PAGE_NAME;
+}
