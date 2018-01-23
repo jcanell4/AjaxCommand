acae72afcb0179c95fd9ada90f2b019f49402305
rclaver <rclaver@xtec.cat>
2018-01-23 11:17:01 +0100

    normalización y control de error

--- commands/user_list/user_list_command.php
+++ commands/user_list/user_list_command.php
@@ -15,9 +15,9 @@ class user_list_command extends abstract_command_class {
         parent::__construct();
         $this->authenticatedUsersOnly = TRUE;
 
-        $this->types[AjaxKeys::KEY_ID] = self::T_STRING;
-        $this->types[AjaxKeys::KEY_DO] = self::T_STRING;
-        $this->types[ProjectKeys::KEY_FILTER] = self::T_STRING;
+        $this->types[PageKeys::KEY_ID] = self::T_STRING;
+        $this->types[PageKeys::KEY_DO] = self::T_STRING;
+        $this->types[PageKeys::KEY_FILTER] = self::T_STRING;
         $this->types[PageKeys::KEY_START_POS] = self::T_INTEGER;
         $this->types[PageKeys::KEY_PROJECT] = self::T_STRING;
     }
@@ -28,12 +28,16 @@ class user_list_command extends abstract_command_class {
 
     protected function process() {
         if (!isset($this->params[AjaxKeys::KEY_DO])) {
-            if (isset($this->params[PageKeys::KEY_PROJECT]) && isset($this->params[AjaxKeys::KEY_ID])){
-                $this->params[AjaxKeys::KEY_DO] = UserListAction::OF_A_PROJECT;
-            }elseif(isset($this->params[AjaxKeys::KEY_ID])){
-                $this->params[AjaxKeys::KEY_DO] = UserListAction::BY_PAGE_PERMSION;
-            }else{
-                $this->params[AjaxKeys::KEY_DO] = UserListAction::BY_NAME;
+            if (isset($this->params[PageKeys::KEY_ID])) {
+                if (isset($this->params[PageKeys::KEY_PROJECT])) {
+                    $this->params[PageKeys::KEY_DO] = UserListAction::OF_A_PROJECT;
+                }elseif(isset($this->params[PageKeys::KEY_FILTER])) {
+                    $this->params[PageKeys::KEY_DO] = UserListAction::BY_NAME;
+                }else {
+                    $this->params[PageKeys::KEY_DO] = UserListAction::BY_PAGE_PERMSION;
+                }
+            }else {
+                throw new IncorrectParametersException();
             }
         }
 
