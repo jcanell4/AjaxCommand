fbc2242bdc03738eb3affbe1a4f050f20b93271c
rclaver <rclaver@xtec.cat>
2018-06-26 14:21:09 +0200

    mejor RequestParameterKeys

--- commands/recent/recent_command.php
+++ commands/recent/recent_command.php
@@ -1,22 +1,23 @@
 <?php
-if (!defined('DOKU_INC')) die();
-
 /**
  * Class recent_command
  * @author Josep Cañellas <jcanell4@ioc.cat>
  */
+if (!defined('DOKU_INC')) die();
+require_once DOKU_INC."lib/lib_ioc/ajaxcommand/abstract_command_class.php";
+
 class recent_command extends abstract_command_class {
 
     public function __construct(){
         parent::__construct();
         $this->types[AjaxKeys::KEY_ID] = self::T_STRING;
-        $this->types[ProjectKeys::SHOW_CHANGES_KEY] = self::T_STRING;
-        $this->types[ProjectKeys::FIRST_KEY] = self::T_ARRAY_KEY;
+        $this->types[RequestParameterKeys::SHOW_CHANGES_KEY] = self::T_STRING;
+        $this->types[RequestParameterKeys::FIRST_KEY] = self::T_ARRAY_KEY;
 
         $defaultValues = [
             AjaxKeys::KEY_ID => '',
-            ProjectKeys::SHOW_CHANGES_KEY => 'both',
-            ProjectKeys::FIRST_KEY => array(0 => ''),
+            RequestParameterKeys::SHOW_CHANGES_KEY => 'both',
+            RequestParameterKeys::FIRST_KEY => array(0 => ''),
         ];
         $this->setParameters($defaultValues);
     }
@@ -31,7 +32,7 @@ class recent_command extends abstract_command_class {
 
     /**
      * @return string (nom del command, a partir del nom de la clase,
-     *                 modificat pels valors de $params per a definir subclasses específiques
+     *                 modificat pels valors de $params per definir subclasses específiques
      *                 amb autoritzacions específiques)
      */
     public function getAuthorizationType() {
