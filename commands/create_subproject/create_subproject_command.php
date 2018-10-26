7ebf46aa87e054f97548a9662f1e80c09b7df747
rclaver <rclaver@xtec.cat>
2018-10-26 11:33:59 +0200

    corrección

--- commands/create_subproject/create_subproject_command.php
+++ commands/create_subproject/create_subproject_command.php
@@ -11,10 +11,10 @@ class create_subproject_command extends project_command {
 
     protected function process() {
         $response = array();
-        $response[ProjectKeys::KEY_ID] = $this->params['new_id'];
+        $response[ProjectKeys::KEY_ID] = str_replace(":", "_", $this->params[ProjectKeys::KEY_ID]);
         $response['urlBase'] = "lib/exe/ioc_ajax.php?call=project";
         $response['query'] = "do=".ProjectKeys::KEY_CREATE_PROJECT;
-        $response['data'] = [ProjectKeys::KEY_ID => str_replace(":", "_", $this->params['new_id']),
+        $response['data'] = [ProjectKeys::KEY_ID => $this->params['new_id'],
                              ProjectKeys::KEY_PROJECT_TYPE => $this->params['new_projectType'],
                              ProjectKeys::KEY_METADATA_SUBSET => $this->params[ProjectKeys::KEY_METADATA_SUBSET]
                             ];
@@ -22,19 +22,13 @@ class create_subproject_command extends project_command {
     }
 
     function getDefaultResponse( $response, &$ret ) {
-        $data = $response['data'];
-        $data[ProjectKeys::KEY_ID] = $this->params['new_id'];
         $ret->addProcessFunction(true, "ioc/dokuwiki/recallCommand",
                                  ['urlBase' => $response['urlBase'],
                                   'method' => "post",
                                   'query' => $response['query'],
-                                  'data' => $data
+                                  'data' => $response['data']
                                  ]
                                 );
     }
 
-    function postResponse($responseData, &$ajaxCmdResponseGenerator) {
-        $responseData['projectExtraData'][ProjectKeys::PROJECT_TYPE] = $responseData['data'][ProjectKeys::PROJECT_TYPE];
-        parent::postResponse($responseData, $ajaxCmdResponseGenerator);
-    }
 }
