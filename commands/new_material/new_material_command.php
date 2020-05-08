3f51ac7d59453765ba08d5a72847f96be20f484e
rclaver <rclaver@xtec.cat>
2020-05-08 11:44:04 +0200

    Nova Class new_material_command

---
+++ commands/new_material/new_material_command.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Class new_material_command
+ */
+if (!defined('DOKU_INC')) die();
+
+class new_material_command extends abstract_command_class {
+
+    public function __construct() {
+        parent::__construct();
+        $this->types[AjaxKeys::KEY_ID] = self::T_STRING;
+        $this->types[ProjectKeys::KEY_TEMPLATE] = self::T_STRING;
+    }
+
+    /**
+     * Retorna la pàgina corresponent a la 'id' i 'rev'.
+     * @return array amb la informació de la pàgina formatada amb 'id', 'ns', 'tittle' i 'content'
+     */
+    protected function process() {
+        PagePermissionManager::updateMyOwnPagePermission($this->authorization->getPermission());
+        $pageModel = new DokuPageModel($this->getPersistenceEngine());
+
+        //sólo se ejecuta si no existe un proyecto en la ruta especificada
+        if (!$pageModel->haveADirProject($this->params[AjaxKeys::KEY_ID])) {
+//            $action = $this->getModelManager()->getActionInstance("CreatePageAction");
+//            $contentData = $action->get($this->params);
+        }
+        if ($contentData)
+            return $contentData;
+        else
+            throw new CantCreatePageInProjectException();
+    }
+
+    /**
+     * Afegeix el contingut com una resposta de tipus HTML_TYPE al generador de respostes passat com argument.
+     * @param array                    $contentData array amb la informació de la pàgina 'id', 'ns', 'tittle' i 'content'
+     * @param AjaxCmdResponseGenerator $responseGenerator
+     * @return void
+     */
+    protected function getDefaultResponse($contentData, &$responseGenerator) {
+        $responseGenerator->addHtmlDoc(
+                            $contentData[AjaxKeys::KEY_ID],
+                            $contentData[AjaxKeys::KEY_NS],
+                            $contentData["title"],
+                            $contentData["content"]
+                        );
+    }
+}
