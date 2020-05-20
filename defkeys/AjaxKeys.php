f7b3ec6499d41c52c8446bd759a108ceae3edb92
rclaver <rclaver@xtec.cat>
2020-05-20 18:36:22 +0200

    const KEY_SECTOK

--- defkeys/AjaxKeys.php
+++ defkeys/AjaxKeys.php
@@ -10,6 +10,7 @@ class AjaxKeys {
     const KEY_ID      = "id";
     const KEY_NS      = "ns";
     const KEY_DO      = "do";
+    const KEY_SECTOK  = 'sectok';
     const CALL_KEY    = 'call';
     const KEY_PROFILE = "profile";
     const FORMAT      = "format";
