5d65fdeb957cbf2ef51f7623d363308ecf9b69c4
Xavier García <xaviergaro.dev@gmail.com>
2020-06-15 19:40:08 +0200

    Changed contentFormat for editorType (for clientside)

--- defkeys/UserStateKeys.php
+++ defkeys/UserStateKeys.php
@@ -8,7 +8,7 @@ if (!defined('DOKU_INC')) die();
 class UserStateKeys extends RequestParameterKeys
 {
     // Editors
-    const KEY_DOJO = "Dojo";
+    const KEY_DOJO = "DOJO";
     const KEY_ACE = "ACE";
 
     // Content Formats
