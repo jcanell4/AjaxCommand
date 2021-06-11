2f9d571daf4ccffec94c1dffb3b91e4fbccfbe42
rclaver <rclaver@xtec.cat>
2021-06-11 12:27:03 +0200

    canvi de get_toc_rest_command a get_toc_page_command: nova versió per ajaxCall

--- commands/get_toc_page/get_toc_page_command.php
+++ commands/get_toc_page/get_toc_page_command.php
@@ -1,29 +1,30 @@
 <?php
 /**
- * Class get_toc_rest_command: Busca la Table Of Contents de una página
+ * Class get_toc_page_command: Busca la Table Of Contents de una página
  * @author rafael <rclaver@ioc.cat>
  */
 if (!defined('DOKU_INC')) die();
 
-class get_toc_rest_command extends abstract_rest_command_class {
+class get_toc_page_command extends abstract_command_class {
 
     public function init($modelManager = NULL) {
         parent::init($modelManager);
         $this->authenticatedUsersOnly = FALSE;
+        $this->throwsEventResponse = FALSE;
     }
 
     /**
      * Busca la Table Of Contents d'una página
      * @return JSON {'html': 'elementos del TOC'}
      */
-    public function processGet() {
+    public function process() {
         $action = $this->getModelManager()->getActionInstance("GetTocAction");
         $toc = $action->get($this->params);
         return $toc;
     }
 
     function getDefaultResponse($response, &$ret) {
-	$ret->setEncodedResponse($response);
+        $ret->setEncodedResponse($response);
     }
 
     /**
