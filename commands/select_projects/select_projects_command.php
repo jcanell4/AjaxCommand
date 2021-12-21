ae7b5b3765193f7f60ca930e3aa26d62a68c6113
Josep <jcanell4@xtec.cat>
2021-12-21 13:31:11 +0100

    Comentaris sobre el codi

--- commands/select_projects/select_projects_command.php
+++ commands/select_projects/select_projects_command.php
@@ -6,6 +6,11 @@
 if (!defined('DOKU_INC')) die();
 
 class select_projects_command extends abstract_admin_command_class {
+    /*
+     * Crec que caldria definir dos commands independents en funció del que demanin (igual que proposo amb els actions):
+     *   1) Command adminform_command o utilform_command o ...
+     *   2) Command específic per fer la petoció especíifica d'acord amb els seu formulari. Concretament ara caldria aquest: select_projects_command
+     */
 
     public function __construct(){
         parent::__construct();
@@ -39,6 +44,9 @@ class select_projects_command extends abstract_admin_command_class {
      * @return string (nom del command per establir autoritzacions específiques)
      */
     public function getAuthorizationType() {
+        /*
+         * JOSEP: Caldrà definir les autoritzacions! Queda pendent.
+         */ 
         return "_none";
     }
 
