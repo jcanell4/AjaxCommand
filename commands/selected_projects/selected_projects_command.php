90221059ec37f9c0d3691d484ec7b6b0850b3599
rclaver <rclaver@xtec.cat>
2022-02-02 12:51:56 +0100

    canvi de nom a selectedProjects

--- commands/selected_projects/selected_projects_command.php
+++ commands/selected_projects/selected_projects_command.php
@@ -1,21 +1,19 @@
 <?php
 /**
- * Class select_projects_command: Mostra una llista filtrada de projectes 
+ * Class selected_projects_command: Mostra una llista filtrada de projectes 
  * @author Rafael <rclaver@xtec.cat>
  */
 if (!defined('DOKU_INC')) die();
 
-class select_projects_command extends abstract_admin_command_class {
+class selected_projects_command extends abstract_admin_command_class {
 
     public function __construct(){
         parent::__construct();
-        $this->types[AjaxKeys::KEY_ID] = self::T_STRING;
     }
 
     protected function process() {
-//        $this->setParameters([AjaxKeys::KEY_ID => "select_projects"]);
-        $this->setParameters([AjaxKeys::KEY_ID => $this->getResponseHandler()->getCommandName()]);
-        $action = $this->getModelManager()->getActionInstance("SelectProjectsAction");
+        $this->setParameters([AjaxKeys::KEY_ID => "selected_projects"]);
+        $action = $this->getModelManager()->getActionInstance("SelectedProjectsAction");
         $response = $action->get($this->params);
         return $response;
     }
