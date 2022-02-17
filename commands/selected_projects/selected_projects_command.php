b31b8e8b61a3b860c2a0435b212216a9c61db86a
rclaver <rclaver@xtec.cat>
2022-02-17 18:50:37 +0100

    Envia missatges als usuaris de projectes seleccionats per rol

--- commands/selected_projects/selected_projects_command.php
+++ commands/selected_projects/selected_projects_command.php
@@ -15,35 +15,17 @@ class selected_projects_command extends abstract_admin_command_class {
         if ($this->params[AjaxKeys::KEY_ID] !== "selected_projects") {
             $this->setParameters([AjaxKeys::KEY_ID => "selected_projects"]);
         }
-        if ($this->params[AjaxKeys::KEY_DO] === "send_message") {
-            $this->setParameters(['type' => "warning",
-                                  'to' => "",
-                                  'send_email' => true
-                                 ]);
-            $action = $this->getModelManager()->getActionInstance("SendMessageAction");
-            $response = $action->get($this->params);
-        }else {
-            $action = $this->getModelManager()->getActionInstance("SelectedProjectsAction");
-            $response = $action->get($this->params);
-        }
+        $action = $this->getModelManager()->getActionInstance("SelectedProjectsAction");
+        $response = $action->get($this->params);
         return $response;
     }
 
     protected function getDefaultResponse($response, &$responseGenerator) {
-        if ($this->params[AjaxKeys::KEY_DO] === "send_message") {
-            if (isset($response['info'])) {
-                $responseGenerator->addInfoDta($response['info']);
-            }
-            foreach ($response['notifications'] as $notification) {
-                $responseGenerator->addNotificationResponse($notification['action'], $notification['params']);
-            }
-        }else {
-            $responseGenerator->addInfoDta(AjaxCmdResponseGenerator::generateInfo(
-                                            RequestParameterKeys::KEY_INFO,
-                                            WikiIocLangManager::getLang("list_projects_showed"),
-                                            $response[AjaxKeys::KEY_ID]
-                                        ));
-        }
+        $responseGenerator->addInfoDta(AjaxCmdResponseGenerator::generateInfo(
+                                        RequestParameterKeys::KEY_INFO,
+                                        WikiIocLangManager::getLang("list_projects_showed"),
+                                        $response[AjaxKeys::KEY_ID]
+                                    ));
     }
 
     //@return string (nom del command per establir autoritzacions específiques)
---
+++ commands/send_message/send_message_command.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * send_message_command: Envia missatges als usuaris de projectes seleccionats per rol
+ * @author Rafael <rclaver@xtec.cat>
+ */
+if (!defined('DOKU_INC')) die();
+
+class send_message_command extends abstract_admin_command_class {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    protected function process() {
+        $this->setParameters(['type' => "warning",
+                              'to' => "",
+                              'send_email' => true
+                             ]);
+        $action = $this->getModelManager()->getActionInstance("SendMessageAction");
+        $response = $action->get($this->params);
+        return $response;
+    }
+
+    protected function getDefaultResponse($response, &$responseGenerator) {
+        if (isset($response['info'])) {
+            $responseGenerator->addInfoDta($response['info']);
+        }
+        foreach ($response['notifications'] as $notification) {
+            $responseGenerator->addNotificationResponse($notification['action'], $notification['params']);
+        }
+    }
+
+    //@return string (nom del command per establir autoritzacions específiques)
+    public function getAuthorizationType() {
+        /*
+         * JOSEP: Caldrà definir les autoritzacions! Queda pendent.
+         */ 
+        return "_none";
+    }
+
+}
