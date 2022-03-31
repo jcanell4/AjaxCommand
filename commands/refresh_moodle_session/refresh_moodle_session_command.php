35f4f5fc75e658d114a45202a602bcdc7b03148c
rclaver <rclaver@xtec.cat>
2022-03-31 19:37:02 +0200

    RefreshMoodleSession (primera versió)

---
+++ commands/refresh_moodle_session/refresh_moodle_session_command.php
@@ -0,0 +1,26 @@
+<?php
+if(!defined('DOKU_INC')) die();
+
+/**
+ * Class refresh_moodle_session_command
+ */
+class refresh_moodle_session_command extends abstract_command_class {
+
+    protected function process() {
+        if (isset($this->params['moodleToken'])) {
+            $action = $this->getModelManager()->getActionInstance("RefreshMoodleSessionAction", FALSE);
+            $contentData = $action->get($this->params);
+        }
+        return $contentData;
+    }
+
+    protected function getDefaultResponse($response, &$ajaxCmdResponseGenerator) {
+        if (isset($response['info'])) {
+            $ajaxCmdResponseGenerator->addInfoDta($response['info']);
+        }
+    }
+
+    public function getAuthorizationType() {
+        return "_none";
+    }
+}
