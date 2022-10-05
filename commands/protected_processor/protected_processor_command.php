f38ea27c005faf6b45241abce5dfced8a15db211
jcanell4 <jcanell4@xtec.cat>
2022-10-06 00:48:57 +0200

    Assegurar que només es ppt executar allò que estigui previst, usant l'ajaxcommand protected_processor

--- commands/protected_processor/protected_processor_command.php
+++ commands/protected_processor/protected_processor_command.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Class user_list_command
  * @author Josep Cañellas <jcanell4@ioc.cat>
  */
 if (!defined('DOKU_INC')) die();
 
 class protected_processor_command extends abstract_command_class {
+    private $protectedCommands;
     public function __construct() {
         parent::__construct();
         $this->authenticatedUsersOnly = TRUE;
@@ -14,6 +14,8 @@ class protected_processor_command extends abstract_command_class {
         $this->types[PageKeys::KEY_ID] = self::T_STRING;
         $this->types[RequestParameterKeys::KEY_COMMAND] = self::T_FUNCTION;
         $this->types[RequestParameterKeys::KEY_PARAMETERS] = self::T_JSON;
+        
+        $this->protectedCommands =["ProjectModelMethodCaller" =>["createTableRAPonderation"]];
     }
 
     protected function getDefaultResponse($response, &$responseGenerator) {
@@ -23,6 +25,11 @@ class protected_processor_command extends abstract_command_class {
     }
 
     protected function process() {
+        if(!isset($this->protectedCommands[$this->params[RequestParameterKeys::KEY_DO]])
+                || $this->protectedCommands[$this->params[RequestParameterKeys::KEY_DO]] !== "*"
+                || !in_array($this->params[RequestParameterKeys::KEY_COMMAND], $this->protectedCommands[$this->params[RequestParameterKeys::KEY_DO]])){
+            throw new Exception("Unsupported action or command");
+        }
         $actionName = $this->params[RequestParameterKeys::KEY_DO];
         $action = $this->getModelManager()->getActionInstance("{$actionName}Action");
         $response = $action->get($this->params);
